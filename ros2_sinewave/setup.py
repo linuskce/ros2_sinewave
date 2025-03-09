0f05f36e3887c37d27ebef4c10cdf09d9d2affb4
Linus Eck <ge75puk@mytum.de>
2025-03-09 21:48:06 +0100

    Added generation of parameter module

--- ros2_sinewave/setup.py
+++ ros2_sinewave/setup.py
@@ -1,28 +1,39 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 from generate_parameter_library_py.setup_helper import generate_parameter_module
 import os
 from glob import glob
 
 package_name = 'ros2_sinewave'
 
+# Generate the parameter module — no manual file moving
+generate_parameter_module(
+    module_name=f"{package_name}.sinewave_parameters",
+    yaml_file=os.path.join('param', 'sinewave_parameters.yaml'),
+    install_base=package_name
+)
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools', 'rclpy', 'std_msgs'],
+    install_requires=['setuptools', 'rclpy', 'std_msgs', 'generate_parameter_library'],
     zip_safe=True,
     maintainer='linus',
     maintainer_email='ge75puk@mytum.de',
-    description='TODO: Package description',
+    description='ROS2 package for publishing and subscribing to sine wave values',
     license='TODO: License declaration',
     tests_require=['pytest'],
+    data_files=[
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'param'), glob('param/*.yaml')),
+    ],
     entry_points={
         'console_scripts': [
+            'sine_publisher = ros2_sinewave.sine_publisher:main',
+            'sine_subscriber = ros2_sinewave.sine_subscriber:main',
         ],
     },
 )
+
+
+
