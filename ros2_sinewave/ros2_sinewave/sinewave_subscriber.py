31b2795a3d444d2973bcc301dc601b0f31aba534
Linus Eck <ge75puk@mytum.de>
2025-03-11 10:50:07 +0100

    Updated subscriber node to include service server and grayscale convertion

--- ros2_sinewave/ros2_sinewave/sinewave_subscriber.py
+++ ros2_sinewave/ros2_sinewave/sinewave_subscriber.py
@@ -3,7 +3,11 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32
+import os 
+import cv2
 
+# Import the custom service definition
+from ros2_sinewave.srv import ConvertImage
 
 class SineWaveSubscriber(Node):
     def __init__(self):
@@ -17,12 +21,55 @@ class SineWaveSubscriber(Node):
             10 
         )
         self.subscription 
-
         self.get_logger().info("SineWaveSubscriber started...")
-
+        
+        # Create a service server for ConvertImage
+        self.srv = self.create_service(
+            ConvertImage,
+            'convert_image', 
+            self.call_image_converting_service
+        )
+        
+        
     def listener_callback(self, msg):
         # Log the received sine wave value
         self.get_logger().info(f"Received: {msg.data:.3f}")
+        
+    def call_image_converting_service(self, request, response):
+        """
+        Service callback for converting an image to grayscale.
+        request: contains request.image_path
+        response: to fill response.grayscale_image_path
+        """
+        input_path = request.image_path
+        if not os.path.exists(input_path):
+            self.get_logger().error(f"File not found: {input_path}")
+            response.grayscale_image_path = ""
+            return response
+
+        # Load the rgb image
+        img = cv2.imread(input_path)
+        if img is None:
+            self.get_logger().error(f"Failed to read image from {input_path}")
+            response.grayscale_image_path = ""
+            return response
+        
+        # Convert image to grayscale
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        
+        # Define the new filepath
+        base, ext = os.path.splitext(input_path)
+        gray_path = base + "_grayscale" + ext
+        
+        # Save grayscale image
+        cv2.imwrite(gray_path, gray_img)
+        self.get_logger().info(f"Saved grayscale image to: {gray_path}")
+        
+        # Fill the response
+        response.grayscale_image_path = gray_path
+        return response
+  
+        
 
 
 def main(args=None):
